d2b15801e7e7eccf0ee8dc9a122cf4f0f7ae60f8
PhaniGITTER <115786449+PhaniGITTER@users.noreply.github.com>
2024-08-14 22:37:31 +0530

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = (
+    <div>
+        <h1>This is Heading 1 </h1>
+        <h2>This is Heading 2</h2>
+        <h3>This is Heading 3 </h3>
+    </div>
+);
+ 
+ReactDOM.render(element, document.getElementById("root"));
